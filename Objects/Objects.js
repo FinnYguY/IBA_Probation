5c3a5b92ebe11e148327fdf85a1eb5a5a4d1a264
FinnYguY <44834982+FinnYguY@users.noreply.github.com>
2020-02-22 20:56:28 +0300

    1

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -1,4 +1,5 @@
 let userObject = {};
+let userArrOfObj = {};
 function createObj(key1, val1, key2, val2) {
   userObject = {
     [key1]: [val1].join(""),
@@ -6,7 +7,28 @@ function createObj(key1, val1, key2, val2) {
   }
   return userObject;
 }
+
+function createArrayOfObjects(key1, val1, key2, val2, key3, val3, key4, val4, key5, val5, key6, val6) {
+  userArrOfObj = [{
+    [key1]: [val1].join(""),
+    [key2]: [val2].join(""),
+  },
+  {
+    [key3]: [val3].join(""),
+    [key4]: [val4].join(""),
+  },
+  {
+    [key5]: [val5].join(""),
+    [key6]: [val6].join(""),
+  }]
+  console.log(userArrOfObj);
+  return userArrOfObj;
+}
 createObj(objkey1.value, objval1.value, objkey2.value, objval2.value);
+createArrayOfObjects(arrobjkey1.value, arrobjval1.value, arrobjkey2.value, arrobjval2.value,
+  arrobjkey3.value, arrobjval3.value, arrobjkey4.value, arrobjval4.value,
+  arrobjkey5.value, arrobjval5.value, arrobjkey6.value, arrobjval6.value);
+
 function isObject(obj) {
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
@@ -16,24 +38,44 @@ function isObject(obj) {
     }
   } return true;
 }
+
 function isFunction(fun) {
   return fun instanceof Function;
 }
+
 function objectKeys(obj) {
   return Object.keys(obj);
 }
+
 function objectValues(obj) {
   return Object.values(obj);
 }
+
 function objectEntries(obj) {
   return Object.entries(obj);
 }
+
 function objToArr(obj) {
   let map = new Map(Object.entries(obj));
   return Array.from(map);
 }
+
 function arrToObj(str) {
   let workArr = str.split(" ");
   let workObj = Object.assign({}, workArr);
   return Object.entries(workObj);
+}
+
+function filterByValue(arr, condition) {
+  let conditionObj = {
+    age: 33,
+  }
+  userArrOfObj = arr.filter(function(item) {
+    for (let key in conditionObj) {
+      if (item[key] === undefined || item[key] != conditionObj[key])
+        return false;
+    }
+  return true;
+  });
+  return userArrOfObj;
 }
