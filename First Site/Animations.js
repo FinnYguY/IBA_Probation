4cdb910a001f2b55e066420fc06144b01b412914
FinnYguY <tema.menschikoff2013165@gmail.com>
2020-03-30 20:13:37 +0300

    Menu add and adaptivity

--- First Site/Animations.js
+++ First Site/Animations.js
@@ -4,8 +4,6 @@ function fadeIn() {
   let blockTop = [];
   for (var i = 0; i < workBlock.length; i++) {
     blockTop[i] = workBlock[i].getBoundingClientRect().top;
-    console.log(`currentBot ${i}: `, currentBot);
-    console.log(`blockTop ${i}: `, blockTop[i]);
     if (currentBot > blockTop[i]) {
       if (workBlock[i].classList.contains('bkgheader')) {
         workBlock[i].style.opacity = 0.3;
